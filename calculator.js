162b80bbc55c5cb71ed454ce5a20da8f371d519e
jpbonch <jp.bonchristiano@gmail.com>
2021-09-17 08:05:03 +0100

    Fixed calculator

--- calculator.js
+++ calculator.js
@@ -12,8 +12,8 @@ function handleCalculation(){
     var calcMatrix = getCalcMatrix();
 
     var inverse = [[Math.round((calcMatrix[1][1] * (1/determinant(calcMatrix))) * 100) / 100, Math.round(-calcMatrix[0][1] * (1/determinant(calcMatrix))* 100) / 100],
-    [Math.round(-calcMatrix[1][0] * (1/determinant(calcMatrix))), Math.round(calcMatrix[0][0] * (1/determinant(calcMatrix)))]];
-    
+    [Math.round(-calcMatrix[1][0] * (1/determinant(calcMatrix)) * 100) /100, Math.round(calcMatrix[0][0] * (1/determinant(calcMatrix)) * 100) /100]];
+
 
     var inverseBoxes = document.querySelectorAll(".inverse");
     inverseBoxes[0].innerText = inverse[0][0];
@@ -22,10 +22,10 @@ function handleCalculation(){
     inverseBoxes[3].innerText = inverse[1][1];
 
     var transposeBoxes = document.querySelectorAll(".transpose");
-    transposeBoxes[0].innerText = calcMatrix[0][0];
-    transposeBoxes[1].innerText = calcMatrix[1][0];
-    transposeBoxes[2].innerText = calcMatrix[0][1];
-    transposeBoxes[3].innerText = calcMatrix[1][1];
+    transposeBoxes[0].innerText = Math.round(calcMatrix[0][0] * 100)/100;
+    transposeBoxes[1].innerText = Math.round(calcMatrix[1][0] * 100)/100;
+    transposeBoxes[2].innerText = Math.round(calcMatrix[0][1] * 100)/100;
+    transposeBoxes[3].innerText = Math.round(calcMatrix[1][1] * 100)/100;
 
     document.getElementById("determinantPe").innerText = "Determinant = " + determinant(calcMatrix);
 }
